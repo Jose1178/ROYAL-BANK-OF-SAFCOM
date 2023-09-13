bf7c7ac33d5202996e6045ca7a1fbdca87a9f45b
Josephmugo <91910681+Jose1178@users.noreply.github.com>
2023-09-13 19:32:08 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Header(){
     return(
         <div className='container p-3 mb-2 mx-auto bg-success text-white'>
-            <h1 className='heading'>ROYAL BANK OF YUTEK</h1>
+            <h1 className='heading'>ROYAL BANK OF SAFCOM</h1>
         </div>
     )
 }
